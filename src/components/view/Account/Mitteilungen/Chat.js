c6de062af79e06b5d8e5527718825eb61500d011
Gera <gera@DevMac.local>
2018-04-15 17:50:21 +0200

    weitere html elemente für chat

--- src/components/view/Account/Mitteilungen/Chat.js
+++ src/components/view/Account/Mitteilungen/Chat.js
@@ -90,6 +90,7 @@ componentWillMount(){
           const keys = Object.keys(message)
 
           return(
+            //chat Übersicht
             <div id="wrapper">
 
               <div id="page-wrapper" >
@@ -121,13 +122,13 @@ componentWillMount(){
                         <div class="inbox-message">
                           <ul>
                             <li>
-                              <a href="chatting.html">
+                              <a href="#messageView">
                                 <div class="message-avatar">
                                   <img src="assets/img/img-1.jpg" alt=""/>
                                 </div>
                                 <div class="message-body">
                                   <div class="message-body-heading">
-                                    <h5>Daniel Dock <span class="unread">ungelesen</span></h5>
+                                    <h5>Harry Trippel <span class="unread">ungelesen</span></h5>
                                     <span>7 hours ago</span>
                                   </div>
                                   <p>Hello, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolor....</p>
@@ -136,13 +137,13 @@ componentWillMount(){
                             </li>
 
                             <li>
-                              <a href="chatting.html">
+                              <a href="#messageView">
                                 <div class="message-avatar">
                                   <img src="assets/img/img-3.jpg" alt=""/>
                                 </div>
                                 <div class="message-body">
                                   <div class="message-body-heading">
-                                    <h5>Daniel Dock <span class="pending">gelesen</span></h5>
+                                    <h5>Herman Mertke <span class="pending">gelesen</span></h5>
                                     <span>7 hours ago</span>
                                   </div>
                                   <p>Hello, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolor....</p>
@@ -156,7 +157,93 @@ componentWillMount(){
                   </div>
                 </div>
               </div>
+
+              <div class="col-sm-9 message_section" id="messageView">
+                <div class="row">
+                  <div class="new_message_head">
+                    <div class="pull-left"><button><i class="fa fa-plus-square-o" aria-hidden="true"></i> New Message</button></div>
+                    <div class="pull-right">
+                       <div class="dropdown">
+                        <button class="dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fa fa-cogs" aria-hidden="true"></i>  Setting
+                        <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu1">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Profile</a></li>
+                        <li><a href="#">Logout</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+
+
+                   <div class="chat_area">
+                     <ul class="list-unstyled" id="chat-scroll">
+                       <li class="left clearfix">
+                        <span class="chat-img1 pull-left">
+                        <img src="https://lh6.googleusercontent.com/-y-MY2satK-E/AAAAAAAAAAI/AAAAAAAAAJU/ER_hFddBheQ/photo.jpg" alt="User Avatar" class="img-circle"/>
+                        </span>
+                        <div class="chat-body1 clearfix">
+                          <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia.</p>
+                          <div class="chat_time pull-right">09:40PM</div>
+                        </div>
+                      </li>
+                      <li class="left clearfix">
+                         <span class="chat-img1 pull-left">
+                         <img src="https://lh6.googleusercontent.com/-y-MY2satK-E/AAAAAAAAAAI/AAAAAAAAAJU/ER_hFddBheQ/photo.jpg" alt="User Avatar" class="img-circle"/>
+                         </span>
+                         <div class="chat-body1 clearfix">
+                          <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia.</p>
+                          <div class="chat_time pull-right">09:40PM</div>
+                         </div>
+                      </li>
+                      <li class="left clearfix">
+                         <span class="chat-img1 pull-left">
+                         <img src="https://lh6.googleusercontent.com/-y-MY2satK-E/AAAAAAAAAAI/AAAAAAAAAJU/ER_hFddBheQ/photo.jpg" alt="User Avatar" class="img-circle"/>
+                         </span>
+                         <div class="chat-body1 clearfix">
+                          <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia.</p>
+                          <div class="chat_time pull-right">09:40PM</div>
+                         </div>
+                      </li>
+                      <li class="left clearfix admin_chat">
+                         <span class="chat-img1 pull-right">
+                         <img src="https://lh6.googleusercontent.com/-y-MY2satK-E/AAAAAAAAAAI/AAAAAAAAAJU/ER_hFddBheQ/photo.jpg" alt="User Avatar" class="img-circle"/>
+                         </span>
+                         <div class="chat-body1 clearfix">
+                          <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia.</p>
+                          <div class="chat_time pull-left">09:40PM</div>
+                         </div>
+                      </li>
+                      <li class="left clearfix admin_chat">
+                         <span class="chat-img1 pull-right">
+                         <img src="https://lh6.googleusercontent.com/-y-MY2satK-E/AAAAAAAAAAI/AAAAAAAAAJU/ER_hFddBheQ/photo.jpg" alt="User Avatar" class="img-circle"/>
+                         </span>
+                         <div class="chat-body1 clearfix">
+                          <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia.</p>
+                          <div class="chat_time pull-left">09:40PM</div>
+                         </div>
+                      </li>
+                    </ul>
+                   </div>
+
+                    <div class="message_write">
+                    <textarea class="form-control" placeholder="type a message"></textarea>
+                    <div class="clearfix"></div>
+                    <div class="chat_bottom"><a href="#" class="pull-left upload_btn"><i class="fa fa-cloud-upload" aria-hidden="true"></i>
+                    Add Files</a>
+                    <a href="#" class="pull-right btn btn-success">
+                    Send</a></div>
+                   </div>
+                </div>
+              </div>
+
             </div>
+
+
+
+
             )
         }
     }
