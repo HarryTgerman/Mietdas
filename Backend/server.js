d61db28d9e4caee6cc27248d36aab6b690bb04ee
Herman Mertke <hermanmertke@hermans-mbp.intern>
2018-05-08 13:09:25 +0200

    payment erstellung hmac_key

--- Backend/server.js
+++ Backend/server.js
@@ -68,6 +68,77 @@ app.post('/payment', (req,res) => {
       "merchantAccount" : "MietDasCOM"
   }
 
-  axios.post('https://pal-test.adyen.com/pal/servlet/Payment/v30/authorise', data, config).then((res)=>{console.log('Das ist die Response', res);}, (err)=>{console.log('Das ist der Error', err);})
+  axios.post('https://pal-test.adyen.com/pal/servlet/Payment/v30/authorise', data, config)
+  .then((res)=>{console.log('Das ist die Response');}, (err)=>{console.log('Das ist der Error');})
 
+  //requestPaymentMethods();
+  signatureCalculation();
 });
+
+requestPaymentMethods = () => {
+  let config = {
+   headers: {
+     'Accept': 'application/json, text/plain, */*',
+     'Content-type':'application/json'
+   }
+ }
+
+ let data = {
+      'countryCode' : 'DE',
+      'currencyCode' : 'EUR',
+      'merchantAccount' : 'MietDasCOM',
+      'merchantReference': 'Test_directory_lookup',
+      'paymentAmount': 2000,
+      'sessionValidity' : '2017-12-25T10%3A31%3A06Z',
+      'skinCode': 'sH9qpMyS',
+      'merchantSig': '94AwPXSxs0ECicXi1UDdKEmdzHQ6rf7EF9CC%2FzUO5Tg%3D'
+  }
+  axios.post('https://pal-test.adyen.com/pal/servlet/Payment/v30/authorise', data, config)
+  .then((res)=>{console.log('Das ist die Response', res);}, (err)=>{console.log('Das ist der Error', err);})
+
+}
+
+signatureCalculation = () => {
+
+  HMAC_KEY = "44782DEF547AAA06C910C43932B1EB0C71FC68D9D0C057550C48EC2ACF6BA056"
+
+  let masterList = [
+    { key :'shopperLocale', val: 'en_GB'},
+    { key :'merchantReference', val: 'paymentTest:143522\\64\\39255'},
+    { key :'merchantAccount', val: 'MietDasCOM'},
+    { key :'sessionValidity', val: '2018-07-25T10:31:06Z'},
+    { key :'shipBeforeDate', val: '2018-07-30'},
+    { key :'paymentAmount', val: '1995'},
+    { key :'currencyCode', val: 'EUR'},
+    { key :'skinCode', val: 'X7hsNDWp'}
+  ]
+
+  masterList = masterList.sort(function (a, b) {
+    return a.key.localeCompare( b.key );
+  });
+
+  console.log('masterList:',masterList);
+
+  //Erstellung von string aus der masterList
+
+  let i;
+  let j;
+  let signingString = "";
+
+  for (i = 0; i < masterList.length; i++) {
+      signingString += masterList[i].key + ":";
+  }
+
+  for (j = 0; j < masterList.length; j++) {
+      signingString += masterList[j].val + ":";
+  }
+  console.log('signing String:',signingString);
+
+  //binäre Konvertierung
+
+  let binary_hmac = Buffer.from(HMAC_KEY, "hex");
+
+  console.log(binary_hmac);
+
+
+}
